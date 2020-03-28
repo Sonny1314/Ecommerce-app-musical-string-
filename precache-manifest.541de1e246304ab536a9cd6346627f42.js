f43b36c69c5053cf5bf9e435876abdcb57a6d68e
Sonny1314 <saranshvashistha@gmail.com>
2020-03-28 21:46:40 +0530

    Updates

---
+++ precache-manifest.541de1e246304ab536a9cd6346627f42.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b542a986a900651b58496c0674e5268",
+    "url": "http//Sonny1314.github.io/react-deploy/index.html"
+  },
+  {
+    "revision": "f189adf0211d906631ac",
+    "url": "http//Sonny1314.github.io/react-deploy/static/css/main.4f797c4d.chunk.css"
+  },
+  {
+    "revision": "10d1fb640dca7b14249a",
+    "url": "http//Sonny1314.github.io/react-deploy/static/js/2.c2fd09c5.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "http//Sonny1314.github.io/react-deploy/static/js/2.c2fd09c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f189adf0211d906631ac",
+    "url": "http//Sonny1314.github.io/react-deploy/static/js/main.cee3f27f.chunk.js"
+  },
+  {
+    "revision": "788ac48684b483e3e0ca",
+    "url": "http//Sonny1314.github.io/react-deploy/static/js/runtime-main.190eb4a8.js"
+  },
+  {
+    "revision": "73a26ff55fdbef240c261f1ced77b399",
+    "url": "http//Sonny1314.github.io/react-deploy/static/media/logo.73a26ff5.png"
+  },
+  {
+    "revision": "e931a86ca6f1af60380297e4f59d9b7d",
+    "url": "http//Sonny1314.github.io/react-deploy/static/media/payment.e931a86c.png"
+  },
+  {
+    "revision": "e83771f4f2fdf1744ce26e89f86bd55b",
+    "url": "http//Sonny1314.github.io/react-deploy/static/media/return.e83771f4.png"
+  },
+  {
+    "revision": "43325e94deec43bf8f39ea3b9f02dcde",
+    "url": "http//Sonny1314.github.io/react-deploy/static/media/slide1.43325e94.jpg"
+  },
+  {
+    "revision": "27381c9583a3c019236a737009aab613",
+    "url": "http//Sonny1314.github.io/react-deploy/static/media/slide2.27381c95.jpg"
+  },
+  {
+    "revision": "c0b54f58dece9d8453c39f246c5c319a",
+    "url": "http//Sonny1314.github.io/react-deploy/static/media/slide3.c0b54f58.jpg"
+  },
+  {
+    "revision": "8b8f7cf6247902e512247031d86e26c9",
+    "url": "http//Sonny1314.github.io/react-deploy/static/media/trust.8b8f7cf6.png"
+  }
+]);
